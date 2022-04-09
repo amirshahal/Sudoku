45cd2f2f3aa253802934bc379e05a7ad745a68a5
amirshahal <amir.shahal@gmail.com>
2022-04-09 23:11:42 +0300

    First git version.

---
+++ SudokuPuzzles.py
@@ -0,0 +1,56 @@
+
+haaretz_20220311_difficult = [
+    [None, None, 3, None, None, None, 6, None, 1],
+    [None, 4, 5, None, 1, 3, None, None, 8],
+    [None, None, None, 7, 4, None, None, None, None],
+
+    [None, None, None, 8, None, None, None, 3, 2],
+    [None, None, 2, None, None, None, 8, None, None],
+    [6, 8, None, None, None, 2, None, None, None],
+
+    [None, None, None, None, 7, 1, None, None, None],
+    [3, None, None, 4, 2, None, 7, 9, None],
+    [7, None, 6, None, None, None, 1, None, None]
+        ]
+
+haaretz_20220311_medium = [
+         [None, None, 6, 3, None, None, None, None, None],
+         [None, 2, 8, 9, None, None, 3, None, 5],
+         [3, 1, None, None, 7, None, None, None, None],
+
+         [6, None, 4, None, 5, None, None, None, None],
+         [None, None, None, None, 1, None, None, None, None],
+         [None, None, None, None, 3, None, 8, None, 2],
+
+         [None, None, None, None, 9, None, None, 4, 7],
+         [5, None, 2, None, None, 7, 6, 1, None],
+         [None, None, None, None, None, 3, 2, None, None]
+         ]
+
+nons = [
+         [None, None, None, None, None, None, None, None, None],
+         [None, None, None, None, None, None, None, None, None],
+         [None, None, None, None, None, None, None, None, None],
+
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+         ]
+
+haaretz_20220401_medium = [
+    [None, None, None, None, None, 5, None, None, None],
+    [None, 6, 7, 1, None, 2, 4, None, None],
+    [None, 5, None, 6, 8, None, None, None, 3],
+
+    [None, 1, None, None, 9, None, 8, 3, 7],
+    [None, 3, None, 5, 7, 6, None, 1, None],
+    [7, 9, 4, None, 1, None, None, 6, None],
+
+    [3, None, None, None, 6, 8, None, 5, None],
+    [None, None, 5, 7, None, 1, 3, 4, None],
+    [None, None, None, 3, None, None, None, None, None],
+]
